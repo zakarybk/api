0ba7c8bb98dfba634b97441795a72a1e303aca99
Zak <19314863+zakarybk@users.noreply.github.com>
2020-03-27 11:35:11 +0000

    Updating language

--- db/languages/active.rb
+++ db/languages/active.rb
@@ -2,11 +2,11 @@
 @languages +=
 [
   {
-    id: 51,
-    name: "C# (Mono 6.6.0.161)",
+    id: 1,
+    name: "C# (Dotnet)",
     is_archived: false,
     source_file: "Main.cs",
-    compile_cmd: "/usr/local/mono-6.6.0.161/bin/mcs %s Main.cs",
-    run_cmd: "/usr/local/mono-6.6.0.161/bin/mono Main.exe"
+    compile_cmd: "/usr/bin/dotnet build",
+    run_cmd: "/usr/bin/dotnet test"
   },
 ]
